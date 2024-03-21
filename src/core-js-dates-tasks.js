80f6beb20117a6442672b01eaa10bc2e40765c00
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-03-21 19:10:09 +0200

    feat: getCountDaysInMonth, getCountDaysOnPeriod done

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -99,8 +99,11 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const myDate = new Date(year, month - 1);
+  const myDateOneMonthNext = new Date(year, month);
+  const millisecondsInOneDay = 1000 * 60 * 60 * 24;
+  return Math.round((myDateOneMonthNext - myDate) / millisecondsInOneDay);
 }
 
 /**
@@ -114,8 +117,11 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const MyDateStart = new Date(dateStart);
+  const MyDateEnd = new Date(dateEnd);
+  const millisecondsInOneDay = 1000 * 60 * 60 * 24;
+  return Math.round((MyDateEnd - MyDateStart) / millisecondsInOneDay) + 1;
 }
 
 /**
